59a124bb365b625681530fda0d19e91055bb6068
Kazi Hasan Ibn Arif EFAZ <32927745+GitFazz@users.noreply.github.com>
2019-05-02 14:37:57 +0600

    Add files via upload

---
+++ angryBIRDS.cpp
@@ -0,0 +1,88 @@
+# include "iGraphics.h"
+
+int x = 300, y = 300, r = 20;
+//char birds[10]="";
+/*
+	function iDraw() is called again and again by the system.
+
+	*/
+
+
+void iDraw() {
+	//place your drawing codes here
+	iSetColor(20, 200, 200);
+	iShowBMP(0,0,"bg.bmp");
+	iShowBMP2(0,50,"gulti.bmp",0);
+
+    iShowBMP2(x,y,"b2.bmp",0);
+
+
+	iShowBMP2(140,00,"b1.bmp",0);
+	iShowBMP2(240,00,"b2.bmp",0);
+	iShowBMP2(350,00,"b3.bmp",0);
+}
+
+/*
+	function iMouseMove() is called when the user presses and drags the mouse.
+	(mx, my) is the position where the mouse pointer is.
+	*/
+void iMouseMove(int mx, int my) {
+
+
+}
+
+/*
+	function iMouse() is called when the user presses/releases the mouse.
+	(mx, my) is the position where the mouse pointer is.
+	*/
+void iMouse(int button, int state, int mx, int my) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		//place your codes here
+		printf("x = %d, y= %d\n",mx,my);
+					x=mx;
+	            y=my;
+
+
+		//if(mx<)
+	}
+	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+		//place your codes here
+
+	}
+}
+
+/*
+	function iKeyboard() is called whenever the user hits a key in keyboard.
+	key- holds the ASCII value of the key pressed.
+	*/
+void iKeyboard(unsigned char key) {
+	if (key == 'q') {
+
+		exit(0);
+	}
+	//place your codes for other keys here
+}
+
+/*
+	function iSpecialKeyboard() is called whenver user hits special keys like-
+	function keys, home, end, pg up, pg down, arraows etc. you have to use
+	appropriate constants to detect them. A list is:
+	GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
+	GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12,
+	GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP,
+	GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT
+	*/
+void iSpecialKeyboard(unsigned char key) {
+
+	if (key == GLUT_KEY_END) {
+		exit(0);
+	}
+	//place your codes for other keys here
+}
+
+
+int main() {
+	//place your own initialization codes here.
+	iInitialize(1000, 600, "ANGRY BIRDS");
+	return 0;
+}
